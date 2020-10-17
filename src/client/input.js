5e777f6d5799fdfa5013af4e00557128169edcf1
Chelovek <chelovekdenis@yandex.ru>
2020-10-17 12:32:05 +0300

    Initial commit

---
+++ src/client/input.js
@@ -0,0 +1,101 @@
+// Learn more about this file at:
+// https://victorzhou.com/blog/build-an-io-game-part-1/#6-client-input-%EF%B8%8F
+import { updateDirection, moveEmit, mouseClickEmit } from './networking';
+
+function onMouseInput(e) {
+  handleInput(e.clientX, e.clientY);
+}
+
+function onTouchInput(e) {
+  const touch = e.touches[0];
+  handleInput(touch.clientX, touch.clientY);
+}
+
+function handleInput(x, y) {
+  const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
+  updateDirection(dir);
+}
+
+let movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+
+function handleInputKey() {
+  moveEmit(movement);
+}
+
+function onMouseDown() {
+  mouseClickEmit(true);
+}
+function onMouseUp() {
+  mouseClickEmit(false);
+}
+
+function onKeyDown(e) {
+  switch (e.keyCode) {
+    case 65: // A
+      movement.left = true
+      break
+    case 87: // W
+      movement.up = true
+      break
+    case 68: // D
+      movement.right = true
+      break
+    case 83: // S
+      movement.down = true
+      break
+  }
+  handleInputKey()
+}
+
+function onKeyUp(e) {
+  switch (e.keyCode) {
+    case 65: // A
+      movement.left = false
+      break
+    case 87: // W
+      movement.up = false
+      break
+    case 68: // D
+      movement.right = false
+      break
+    case 83: // S
+      movement.down = false
+      break
+  }
+  handleInputKey()
+}
+
+export function startCapturingInput() {
+  window.addEventListener('mousemove', onMouseInput);
+  // window.addEventListener('click', onMouseClick);
+  window.addEventListener('mousedown', onMouseDown);
+  window.addEventListener('mouseup', onMouseUp);
+  window.addEventListener('touchstart', onTouchInput);
+  window.addEventListener('touchmove', onTouchInput);
+  window.addEventListener('keydown', onKeyDown);
+  window.addEventListener('keyup', onKeyUp);
+}
+
+export function stopCapturingInput() {
+  window.removeEventListener('mousemove', onMouseInput);
+  // window.removeEventListener('click', onMouseClick);
+  window.addEventListener('mousedown', onMouseDown);
+  window.addEventListener('mouseup', onMouseUp);
+  window.removeEventListener('touchstart', onTouchInput);
+  window.removeEventListener('touchmove', onTouchInput);
+  window.removeEventListener('keydown', onKeyDown);
+  window.removeEventListener('keyup', onKeyUp);
+}
+
+
+// document.addEventListener('keydown', event => {
+//
+// })
+// document.addEventListener('keyup', event => {
+//
+// })
---
+++ src/server/app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const path = require('path')
+const socket = require('socket.io')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+
+const Constants = require('../shared/constants');
+const Game = require('./game');
+const webpackConfig = require('../../webpack.dev.js')
+
+const app = express()
+
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'development') {
+    const compiler = webpack(webpackConfig)
+    app.use(webpackDevMiddleware(compiler))
+} else {
+    app.use(express.static('dist'))
+}
+
+
+const port = process.env.PORT || 3000
+const server = app.listen(port)
+console.log(`Server listening on port ${port}`)
+
+const io = socket(server)
+
+
+io.on('connection', socket => {
+    console.log('Player connected!', socket.id);
+
+    socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
+    socket.on(Constants.MSG_TYPES.INPUT, handleInput);
+    socket.on("movement", movement);
+    socket.on("mouse_click", mouseClick);
+    socket.on('disconnect', onDisconnect);
+});
+
+// Setup the Game
+const game = new Game();
+
+function joinGame(username) {
+    game.addPlayer(this, username);
+}
+
+function handleInput(dir) {
+    game.handleInput(this, dir);
+}
+
+function onDisconnect() {
+    game.removePlayer(this);
+}
+
+function movement(move) {
+    game.ifMovement(this, move)
+    // console.log(move);
+}
+
+function mouseClick(click) {
+    game.ifMouseClick(this, click)
+}
